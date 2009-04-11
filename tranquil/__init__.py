f8dd2c1e44cf0411189e159f724f85c50804e63f
Gabriel <g2p.code@gmail.com>
2009-04-11 19:43:19 +0200

    If django_sqlalchemy is configured as the database engine, use it.

--- tranquil/__init__.py
+++ tranquil/__init__.py
@@ -24,7 +24,12 @@ class EngineCache(object):
 	def __init__(self):
 		from django.conf import settings
 		self.__dict__ = self.__shared_state
-		if self.engine is None:
+		if self.engine is not None:
+			return
+		if settings.DATABASE_ENGINE == 'django_sqlalchemy.backend':
+			from django_sqlalchemy import backend
+			self.engine = backend.engine
+		else:
 			options = {
 				'protocol': self._mappings.get( settings.DATABASE_ENGINE ),
 				'name': settings.DATABASE_NAME,
@@ -42,9 +47,9 @@ class EngineCache(object):
 				else:
 					url = re.sub( '{%s}' % p, options[p], url )
 			self.engine = create_engine( url )
-			self.meta = MetaData(bind=self.engine,reflect=True)
-			self.Session = sessionmaker( bind=self.engine, autoflush=True, transactional=True )
-			self.importer = Importer(self.meta)
+		self.meta = MetaData(bind=self.engine,reflect=True)
+		self.Session = sessionmaker( bind=self.engine, autoflush=True, transactional=True )
+		self.importer = Importer(self.meta)
 
 cache = EngineCache()
 engine = cache.engine
